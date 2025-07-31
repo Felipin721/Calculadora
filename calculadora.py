5e055b0402399ad2ac60c41feaebd675635a05cd
Felipin721 <rottabfelipe@gmail.com>
2025-07-31 08:20:02 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+#calculadora simples de Felipe de Bitencourt Rotta N°10
+
+print("Vamos somar dois números!")
+num1 = float(input("Digite o seu primeiro número: "))
+num2 = float(input("Digite o seu segundo número: "))
+soma = num1 + num2
+print(f"A soma de {num1} e {num2} é {soma}.")
